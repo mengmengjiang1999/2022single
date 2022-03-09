94aedf242ad5b1e486af38606243a3def9fa7949
陈张萌 <chenzm17@mails.tsinghua.edu.cn>
2022-03-09 23:55:35 +0800

    generate 205 files

--- main.py
+++ main.py
@@ -7,50 +7,79 @@ from flask_sqlalchemy import SQLAlchemy  # 导入扩展类
 
 app = Flask(__name__)
 
-@app.route('/algorithm/dijkstra')
-def dijkstra():
-    if request.method == 'GET':
-        N = request.form['N']
+# @app.route('/algorithm/dijkstra')
+# def dijkstra():
+#     if request.method == 'GET':
+#         N = request.form['N']
 
-        from run import run
+#         from run import run
 
-        filepath_in, filepath_ans, filepath_problem = run()
+#         filepath_in, filepath_ans, filepath_problem = run()
 
-        file_in = open(filepath_in,"r")
-        data_in = file_in.read()
-        file_in.close()
+#         file_in = open(filepath_in,"r")
+#         data_in = file_in.read()
+#         file_in.close()
 
-        file_ans = open(filepath_ans,"r")
-        data_ans = file_ans.read()
-        file_ans.close()
+#         file_ans = open(filepath_ans,"r")
+#         data_ans = file_ans.read()
+#         file_ans.close()
 
-        file_problem = open(filepath_problem,"r")
-        data_problem = file_problem.read()
-        file_problem.close()
+#         file_problem = open(filepath_problem,"r")
+#         data_problem = file_problem.read()
+#         file_problem.close()
 
-        data = {
-            'N': N,
-            'data_in': data_in,
-            'data_ans': data_ans,
-            'data_problem': data_problem,
-        }
-        return jsonify(data)
-    elif request.method == 'POST':
-        return 'haha'
-
-# with app.test_request_context():
-#     print(url_for('index'))
-#     print(url_for('hello', next='/'))
-#     print(url_for('profile', username='John Doe'))
+#         data = {
+#             'N': N,
+#             'data_in': data_in,
+#             'data_ans': data_ans,
+#             'data_problem': data_problem,
+#         }
+#         return jsonify(data)
+#     elif request.method == 'POST':
+#         return 'haha'
 
 
-if __name__ == '__main__':
-    app.debug = True
-    app.run()
-    app.run(debug = True)
+@app.route("/download/<filepath>", methods=['GET'])
+def download_file(filepath):
+    return app.send_static_file(filepath)  
 
-    # 进行预编译
+
+def genenrate_files():
+# 进行预编译
     from run import pre_compile
     pre_compile()
 
     # 生成300个文件
+    from run import run
+
+    file_codelist = open('list.txt','r')
+
+    codes = file_codelist.readlines()
+
+    print(len(codes))
+
+    for i in range(len(codes)):
+        print(i)
+
+        filepath_in, filepath_ans, filepath_problem = run()
+
+        # 得到markdown并压缩
+        os.system('markdown-pdf '+filepath_problem)
+        filepath_pdf = './data/problem.pdf'
+
+        # 复制并保存
+        filepath_problem_this = codes[i].strip()+'.pdf'
+        os.system('cp '+filepath_pdf + ' ./static/'+filepath_problem_this)
+
+        # 复制并保存输入文件和答案
+        filepath_in_this = codes[i].strip()+'.in'
+        os.system('cp '+filepath_in + ' ./data/'+filepath_in_this)
+        filepath_ans_this = codes[i].strip()+'.ans'
+        os.system('cp '+filepath_ans + ' ./data/'+filepath_ans_this)
+
+
+if __name__ == '__main__':
+    
+    app.debug = True
+    app.run()
+    app.run(debug = True)
