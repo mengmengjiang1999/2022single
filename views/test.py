7aee551bd71d86d56c766b4ca8063751aa4a345e
陈张萌 <chenzm17@mails.tsinghua.edu.cn>
2022-06-08 01:57:21 +0800

    Add access count statistics

--- views/test.py
+++ views/test.py
@@ -1,3 +1,4 @@
+import os
 from flask import Blueprint
 bluetest=Blueprint('test',__name__)   #蓝图的对象的名称=Blueprint('自定义蓝图名称',__name__) 
 
@@ -12,3 +13,20 @@ def index():
 @bluetest.route("/api", methods=['GET'])
 def timenow():
     return "111"
+
+@bluetest.route("/access", methods=['GET'])
+def accesss_count():
+    with open("access_count.txt", "r+") as f:
+        count = f.read()
+        if not count:
+            count = 1
+        else:
+            count = int(count) + 1
+        print("Access count:", count)
+        f.seek(0)
+        f.write(str(count))
+    return str(count)
+
+if not os.path.exists("access_count.txt"):
+    with open("access_count.txt", "w") as f:
+        f.write("0")
